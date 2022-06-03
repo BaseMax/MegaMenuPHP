12ecda3f44dc540ba89273dc9da942b4bde63ac8
Max Base <MaxBaseCode@Gmail.Com>
2022-06-03 09:17:47 +0430

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,12 +4,12 @@ $db = new PDO("mysql:host=localhost;dbname=megamenu", "root", "");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-function menu($parent_id = null)
+// Function: menu
+function menu(PDO $db, ?int $parent_id = null)
 {
-    global $db;
     $items = [];
 
-    $sql = "SELECT `menu_item`.* FROM `menu` INNER JOIN `menu_item` ON `menu_item`.`id` = `menu`.`menu_id` WHERE parent_id ";
+    $sql = "SELECT `menu_item`.* FROM `menu` INNER JOIN `menu_item` ON `menu_item`.`id` = `menu`.`menu_id` WHERE `menu`.`parent_id`";
     if ($parent_id === null)
         $stmt = $db->prepare("$sql IS NULL;");
     else
@@ -18,12 +18,12 @@ function menu($parent_id = null)
     $items = $stmt->fetchAll(PDO::FETCH_NAMED);
 
     foreach ($items as &$row) {
-        $row["children"] = menu($row["id"]);
+        $row["children"] = menu($db, $row["id"]);
         if ($row["children"] === []) unset($row["children"]);
     }
 
     return $items;
 }
 
-$items = menu();
+$items = menu($db, null);
 print_r($items);
